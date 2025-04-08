889d8fb74b73def676c3bbf552d9300e1861bd7d
Tohid Khan <tohidkhan9050482152@gmail.com>
2025-04-08 21:54:39 +0530

    TOHID-AI

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,20 +1,26 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
-  let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-  m.reply(
-    `▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` +
-      users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` +
-      '\n└──✪ 𝚃𝙾𝙷𝙸𝙳-𝙰𝙸 ┃ ᴮᴼᵀ ✪──',
-    null,
-    {
-      mentions: users,
+    const botNumber = conn.user.jid;
+    const users = participants.map(u => u.id).filter(v => v !== conn.user.jid);
+    if (m.sender !== botNumber && !isOwner && !isAdmin) {
+        m.reply("🛡️ This command is only for *Group Admins*");
+        return;
     }
-  )
-}
 
-handler.help = ['tagall']
-handler.tags = ['group']
-handler.command = ['tagall']
-handler.admin = true
-handler.group = true
+    const groupInfo = `┃🇮🇳⊹ 𝗚𝗥𝗢𝗨𝗣 : *${groupMetadata.subject}*\n\n`;
+    const membersInfo = `┃🇮🇳⊹ 𝗠𝗘𝗠𝗕𝗘𝗥𝗦 : *${participants.length}*${text ? `\n┃🇮🇳⊹ 𝗠𝗘𝗦𝗦𝗔𝗚𝗘 : ${text}\n` : ''}`;
+    const mentions = users.map(v => '┃🇮🇳⊹ @' + v.replace(/@.+/, '')).join`\n`;
+    const footer = '\n└──✪ 𝚃𝙾𝙷𝙸𝙳-𝙰𝙸 ┃ ᴮᴼᵀ ✪──';
 
-export default handler
+    const tagAllMessage = `${groupInfo}${membersInfo}\n\n┌───⊷ 𝗠𝗘𝗡𝗧𝗜𝗢𝗡𝗦\n${mentions}${footer}`;
+
+
+    m.reply(tagAllMessage, null, { mentions: users });
+};
+
+handler.help = ['tagall'];
+handler.tags = ['group'];
+handler.command = ['tagall', 'invo'];
+handler.admin = false; 
+handler.group = true; 
+
+export default handler;
