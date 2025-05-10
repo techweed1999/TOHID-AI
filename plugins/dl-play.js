15cdeacf16c5e47a87918d2e4ad2b7fbc9d836c7
Tohid Khan <tohidkhan9050482152@gmail.com>
2025-05-10 10:50:54 +0530

    Add files via upload

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -14,8 +14,7 @@ let handler = async (m, { conn, text, botname }) => {
 
     let link = video.url;
     let apis = [
-      `https://apis.davidcyriltech.my.id/youtube/mp3?url=${link}`,
-      `https://api.ryzendesu.vip/api/downloader/ytmp3?url=${link}`
+      `https://api-dark-shan-yt.koyeb.app/download/ytmp3_v5?url=${link}`
     ];
 
     for (const api of apis) {
--- plugins/dl-song.js
+++ plugins/dl-song.js
@@ -14,9 +14,7 @@ let handler = async (m, { conn, text }) => {
 
     let link = video.url;
     let apis = [
-      `https://apis.davidcyriltech.my.id/youtube/mp3?url=${link}`,
-      `https://api.ryzendesu.vip/api/downloader/ytmp3?url=${link}`,
-      `https://api.akuari.my.id/downloader/youtubeaudio?link=${link}`
+      `https://api-dark-shan-yt.koyeb.app/download/ytmp3_v5?url=${link}`
     ];
 
     // Fetch from the first working API
