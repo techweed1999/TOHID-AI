b61e9b9679d1897f38ed12340fc035e3e56a0a75
techweed1999 <weedtech56@gmail.com>
2025-06-06 09:57:16 -0400

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 //
 global.botname = process.env.BOTNAME || 'TOHID-AI';
 global.pairingNumber = process.env.BOT_NUMBER || '918930358452' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.SESSION_ID = process.env.SESSION_ID || 'Yq82DCRZ#K6IZJ1OtTZBAkJ1N9FeM9-nPQ05oTk-ysMVdmBL5cvw' ;  // put your session id here
 
 global.mods = []
 global.prems = []
